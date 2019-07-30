25cc6ff2f13b8ba31bd3e95a21725b3a79b8c2f9
kalpeshkundanani <kalpeshbkundanani@gmail.com>
2019-07-30 22:09:38 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Image from assets"),
+        ),
+        body: Image.asset('assets/images/image.png'),
+      ),
+    );
+  }
+}
